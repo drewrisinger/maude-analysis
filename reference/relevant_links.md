ef0c99c567e42b97bf99442509d0d6326de5c0b7
Drew Risinger <drisinger+github@gmail.com>
2018-04-02 10:17:34 -0400

    Add more analysis and summary of data in FDA MAUDE files.

--- reference/relevant_links.md
+++ reference/relevant_links.md
@@ -15,3 +15,21 @@ TODO: insert links to relevant papers here
 
 
 TODO: add other types of documentation/links as necessary
+
+# Summary of Data File types
+## FOIDev20XX
+Basic data about the device. Includes manufacturer, location, date of failure, etc.
+
+## FOIText20XX
+Text report of the failure, and what happened. Date of report
+
+## MDRFOI
+Report on when the failure occurred, the manufacturer, etc. No text/explicit problem code
+
+## Patient
+Information about sequence number? Not relevant.
+
+## FOIDevProblem
+Linking MDR events to a specific problem code.
+
+
--- src/maude_interface.py
+++ src/maude_interface.py
@@ -3,6 +3,8 @@ from typing import Iterable
 import logging
 import os
 
+from pandas.errors import ParserError
+
 
 def maude_to_pandas(maude_file_path: str) -> pandas.DataFrame:
     """
@@ -11,16 +13,25 @@ def maude_to_pandas(maude_file_path: str) -> pandas.DataFrame:
     :return: Pandas dataframe containing all the data
     """
     logging.debug("Reading file {}".format(maude_file_path))
-    return pandas.read_csv(maude_file_path, sep='|', encoding='ANSI')
+    try:
+        return_frame = pandas.read_csv(maude_file_path, sep='|', encoding='ANSI')
+    except ParserError as e:
+        check_bad_csv(maude_file_path)
+        raise e
+
+    return return_frame
 
 
 def convert_problem_code_to_description(devproblem_db):
     #todo
     pass
 
-
+######################################################
+# DEPRECATED. DEPRECATED. DEPRECATED.
+######################################################
 def add_data_to_mdr(mdr_base: pandas.DataFrame, other_df_files: Iterable[str], base_key: str ="MDR_REPORT_KEY") -> pandas.DataFrame:
     """
+    DEPRECATED. DO NOT USE
     Merges other databases into a master record of all information.
     :return: merged Panda Dataframe
     """
@@ -31,13 +42,16 @@ def add_data_to_mdr(mdr_base: pandas.DataFrame, other_df_files: Iterable[str], b
         except KeyError:
             logging.warning("File {} does not have key {}".format(df_loc, base_key))
     return merged_df
+######################################################
+# End DEPRECATED function.
+######################################################
 
 
 def compile_maude_database(directory_path: str, base_file_name: str) -> pandas.DataFrame:
     """
     Compiles several txt MAUDE database files into a single database.
     :param directory_path: Directory where the files are stored
-    :param base_file_name: The initial file name to construct the database from. Will only use MDR keys from this
+    :param base_file_name: The initial file name to construct the database from. Will only use MDR keys from this. Should start with mdr
     :return: DataFrame compiled from all these files
     """
     if not directory_path.endswith('/'):
@@ -50,11 +64,11 @@ def compile_maude_database(directory_path: str, base_file_name: str) -> pandas.D
     entire_dataset = maude_to_pandas(directory_path + base_file_name)
 
     for file in directory_files:
-        if file.endswith(".txt"):
+        file_path = os.path.join(directory_path, file)
+        if file_path.endswith(".txt") and not os.path.samefile(file_path, base_file_path) and not file.startswith("mdr"):
             logging.debug("Attempting to add file {} to dataset".format(file))
             try:
-                entire_dataset.merge(entire_dataset, maude_to_pandas(os.path.join(directory_path,file)),
-                                     how='left', on=mdr_base_key)
+                entire_dataset.merge(maude_to_pandas(file_path), on=mdr_base_key)
             except KeyError:
                 logging.warning("File {} does not have key {}".format(file, mdr_base_key))
 
@@ -63,3 +77,19 @@ def compile_maude_database(directory_path: str, base_file_name: str) -> pandas.D
     logging.debug("Final dataset shape: {}\nColumn headers: {}".format(entire_dataset.shape, list(entire_dataset)))
 
     return entire_dataset
+
+
+def check_bad_csv(check_file_path: str):
+    import csv
+    import sys
+    csv.field_size_limit(sys.maxsize)
+    with open(check_file_path, 'r') as f:
+        reader = csv.reader(f, delimiter='|', quoting=csv.QUOTE_NONE)
+        first_row = next(reader)
+        default_row_length = len(first_row)
+        i = 2
+        for row in reader:
+            if len(row) is not default_row_length:
+                logging.error("Error in row {}. Should have {} cols, has {}. "
+                              "Row is: \n{}".format(i, len(first_row), len(row), ", ".join(row)))
+            i = i+1
