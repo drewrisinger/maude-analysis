3bf437269633d7bc23037c547e73200714890ba1
Andrew Risinger <drisingr@umiacs.umd.edu>
2018-04-06 02:47:09 -0400

    Test code and add text processing

--- src/maude_interface.py
+++ src/maude_interface.py
@@ -1,20 +1,39 @@
+import string
 import pandas
 import numpy as np
 from typing import Iterable, Any
 import logging
 import os
 import csv
-#import sys
+# import sys
 
 from pandas.errors import ParserError
 from pandas.api.types import CategoricalDtype
 from functools import partial
 
-true_vals = [True, "True", "Y"]
-false_vals = [False, "False", "N"]
+true_vals = ["True", "Y"]
+false_vals = ["False", "N"]
 read_maude_csv = partial(pandas.read_csv, delimiter='|', encoding='ANSI',
-                                       error_bad_lines=False, quoting=csv.QUOTE_NONE, true_values=true_vals,
-                                       false_values=false_vals, skipinitialspace=True)
+                         error_bad_lines=False, quoting=csv.QUOTE_NONE, true_values=true_vals,
+                         false_values=false_vals, skipinitialspace=True)
+
+reporter_occupation_map = {'*': np.nan, '000': np.nan, '001': 'PHYSICIAN', '002': 'NURSE', '0HP': 'HEALTH PROFESSIONAL',
+                           '0LP': 'LAY USER/PATIENT', '100': 'OTHER HEALTH CARE PROFESSIONAL', '101': 'AUDIOLOGIST',
+                           '102': 'DENTAL HYGIENIST', '103': 'DIETICIAN', '104': 'EMERGENCY MEDICAL TECHNICIAN',
+                           '105': 'MEDICAL TECHNOLOGIST', '106': 'NUCLEAR MEDICINE TECHNOLOGIST',
+                           '107': 'OCCUPATIONAL THERAPIST', '108': 'PARAMEDIC', '109': 'PHARMACIST',
+                           '110': 'PHLEBOTOMIST', '111': 'PHYSICAL THERAPIST',
+                           '112': 'PHYSICIAN ASSISTANT', '113': 'RADIOLOGIC TECHNOLOGIST',
+                           '114': 'RESPIRATORY THERAPIST',
+                           '115': 'SPEECH THERAPIST', '116': 'DENTIST', '300': 'OTHER CAREGIVERS',
+                           '301': 'DENTAL ASSISTANT',
+                           '302': 'HOME HEALTH AIDE', '303': 'MEDICAL ASSISTANT', '304': 'NURSING ASSISTANT',
+                           '305': 'PATIENT', '306': 'PATIENT FAMILY MEMBER OR FRIEND', '307': 'PERSONAL CARE ASSISTANT',
+                           '400': 'SERVICE AND TESTING PERSONNEL', '401': 'BIOMEDICAL ENGINEER',
+                           '402': 'HOSPITAL SERVICE TECHNICIAN',
+                           '403': 'MEDICAL EQUIPMENT COMPANY TECHNICIAN/REPRESENTATIVE', '404': 'PHYSICIST',
+                           '405': 'SERVICE PERSONNEL', '499': 'DEVICE UNATTENDED', '500': 'RISK MANAGER',
+                           '600': 'ATTORNEY', '999': np.nan, 'NA': np.nan, 'NI': np.nan, 'UNK': np.nan}
 
 
 def maude_to_pandas(maude_file_path: str, dtype: Any = "str") -> pandas.DataFrame:
@@ -33,41 +52,58 @@ def maude_to_pandas(maude_file_path: str, dtype: Any = "str") -> pandas.DataFram
 
     return return_frame
 
+
 def read_mdr_file(maude_file_path: str) -> pandas.DataFrame:
-    mdr_col_types = {"MDR_REPORT_KEY": np.int, "REPORT_SOURCE_CODE": CategoricalDtype(categories=["P", "U", "D", "M"], ordered=False),
-                     "INITIAL_REPORT_TO_FDA": CategoricalDtype(categories=["Y", "N", "U", "*"]), "EVENT_TYPE": CategoricalDtype(categories=["D", "IN", "IL", "IJ", "M", "O", *])}
+    mdr_col_types = {"MDR_REPORT_KEY": np.int64,
+                     "REPORT_SOURCE_CODE": CategoricalDtype(categories=["P", "U", "D", "M"], ordered=False),
+                     "INITIAL_REPORT_TO_FDA": CategoricalDtype(categories=["Y", "N", "U", "*"]),
+                     "EVENT_TYPE": CategoricalDtype(categories=["D", "IN", "IL", "IJ", "M", "O"])}
     date_columns = ['DATE_RECEIVED', 'DATE_REPORT', 'DATE_OF_EVENT', 'DATE_FACILITY_AWARE', 'REPORT_DATE',
                     'DATE_REPORT_TO_FDA', 'DATE_REPORT_TO_MANUFACTURER', 'DATE_MANUFACTURER_RECEIVED',
                     'DEVICE_DATE_OF_MANUFACTURE', 'DATE_ADDED', 'DATE_CHANGED']
-    try:
-        return_frame = read_maude_csv(maude_file_path, dtype=mdr_col_types, parse_dates=date_columns)
+    used_cols = ['MDR_REPORT_KEY', 'REPORT_SOURCE_CODE', 'DATE_RECEIVED', 'ADVERSE_EVENT_FLAG', 'PRODUCT_PROBLEM_FLAG',
+                 'REPORTER_OCCUPATION_CODE', 'DATE_REPORT', 'EVENT_TYPE']
+    return_frame = read_maude_csv(maude_file_path, dtype=mdr_col_types, usecols=used_cols,
+                                  parse_dates=['DATE_RECEIVED', 'DATE_REPORT'])
 
     return return_frame
-    #todo: parse
 
 
 def read_dev_file(maude_file_path: str) -> pandas.DataFrame:
     dev_col_types = {"MDR_REPORT_KEY": np.int}
-    date_cols = ['DATE_RECEIVED', 'DATE_RECEIVED', 'EXPIRATION_DATE_OF_DEVICE', 'DATE_RETURNED_TO_MANUFACTURER', 'BASELINE_DATE_FIRST_MARKETED', 'BASELINE_DATE_CEASED_MARKETING']
+    date_cols = ['DATE_RECEIVED', 'DATE_RECEIVED', 'EXPIRATION_DATE_OF_DEVICE', 'DATE_RETURNED_TO_MANUFACTURER',
+                 'BASELINE_DATE_FIRST_MARKETED', 'BASELINE_DATE_CEASED_MARKETING']
     # todo: use na_values for {"DATE_REMOVED_FLAG": "A", "DEVICE_OPERATOR": ["*", "NA", "NI", "UNK"]}
-    # note: for sequence number, to strip (after splitting, use: " ".split().lstrip(string.digits + string.whitespace + string.punctuation + "IAO")
-    #todo: parse
-
-    try:
-        return_frame = read_maude_csv(maude_file_path, dtype=dev_col_types, parse_dates=date_cols)
-
+    # na_vals = {"DATE_REMOVED_FLAG": "A", "DEVICE_OPERATOR": ["*", "NA", "NI", "UNK"]}
+    na_vals = ["A", "*", "NA", "NI", "UNK"]
+    used_cols = ['MDR_REPORT_KEY', 'DEVICE_EVENT_KEY', 'BRAND_NAME', 'GENERIC_NAME', 'MANUFACTURER_D_NAME', 'MODEL_NUMBER']
+    return_frame = read_maude_csv(maude_file_path, dtype=dev_col_types, usecols=used_cols, na_values=na_vals)
+    # parse_date = date_cols,
     return return_frame
 
 
 def read_patient_file(maude_file_path: str) -> pandas.DataFrame:
     date_cols = ["DATE_RECEIVED"]
-    # todo: read
+    # note: for sequence number, to strip (after splitting, use: " ".split().lstrip(string.digits + string.whitespace + string.punctuation)
+    used_cols = ['MDR_REPORT_KEY', 'SEQUENCE_NUMBER_OUTCOME']
+    return_frame = read_maude_csv(maude_file_path, usecols=used_cols)
+    return_frame['SEQUENCE_NUMBER_OUTCOME'] = return_frame['SEQUENCE_NUMBER_OUTCOME'].str.split(";"). \
+        lstrip(string.digits + string.whitespace + string.punctuation)
+    return return_frame
+
+
+def read_maude_text_file(maude_file_path: str)->pandas.DataFrame:
+    used_cols = ['MDR_REPORT_KEY', 'FOI_TEXT']
+    return_frame = read_maude_csv(maude_file_path, usecols=used_cols)
+
+    return return_frame
 
 
 ######################################################
 # DEPRECATED. DEPRECATED. DEPRECATED.
 ######################################################
-def add_data_to_mdr(mdr_base: pandas.DataFrame, other_df_files: Iterable[str], base_key: str ="MDR_REPORT_KEY") -> pandas.DataFrame:
+def add_data_to_mdr(mdr_base: pandas.DataFrame, other_df_files: Iterable[str],
+                    base_key: str = "MDR_REPORT_KEY") -> pandas.DataFrame:
     """
     DEPRECATED. DO NOT USE
     Merges other databases into a master record of all information.
@@ -80,6 +116,8 @@ def add_data_to_mdr(mdr_base: pandas.DataFrame, other_df_files: Iterable[str], b
         except KeyError:
             logging.warning("File {} does not have key {}".format(df_loc, base_key))
     return merged_df
+
+
 ######################################################
 # End DEPRECATED function.
 ######################################################
@@ -105,23 +143,28 @@ def compile_maude_database(directory_path: str, reference_directory_path: str, b
     problem_code_key = "DEVICE_PROBLEM_CODE"
     error_code_header = 'ERR_TYPE'
 
-    # TODO: convert columns to appropriate datatypes
     directory_files = os.listdir(directory_path)
     base_file_path = directory_path + base_file_name
-    logging.debug("Constructing database from base file: {}".format(base_file_path))
-    entire_dataset = maude_to_pandas(directory_path + base_file_name)
+    logging.debug("Constructing database from base MDR file: {}".format(base_file_path))
+    entire_dataset = read_mdr_file(directory_path + base_file_name)
 
     for file in directory_files:
         file_path = os.path.join(directory_path, file)
-        if file_path.endswith(".txt") and not os.path.samefile(file_path, base_file_path) and not file.startswith("mdr"):
+        if file_path.endswith(".txt") and not os.path.samefile(file_path, base_file_path) and not file.startswith(
+                "mdr"):
             logging.debug("Attempting to add file {} to dataset".format(file))
             try:
-                merge_file = maude_to_pandas(file_path)
+                if file.startswith('foitext'):
+                    merge_file = read_maude_text_file(file_path)
+                elif file.startswith('foidev'):
+                    merge_file = read_dev_file(file_path)
+                elif file.startswith('patient'):
+                    merge_file = read_patient_file(file_path)
+                else:
+                    merge_file = maude_to_pandas(file_path)
                 entire_dataset.merge(merge_file, on=mdr_base_key)
             except KeyError:
                 logging.warning("File {} does not have key {}".format(file, mdr_base_key))
-            finally:
-                del merge_file
 
     # entire_dataset = add_data_to_mdr(entire_dataset, directory_files)
     problem_codes = maude_to_pandas(problem_code_path)
@@ -134,6 +177,32 @@ def compile_maude_database(directory_path: str, reference_directory_path: str, b
     logging.debug("Final dataset shape: {}\nColumn headers: {}".format(entire_dataset.shape, list(entire_dataset)))
 
     return entire_dataset
+
+
+def get_all_text_data(directory_path: str, years: Iterable = None) -> pandas.DataFrame:
+    if not directory_path.endswith('/'):
+        raise ValueError("Argument directory path ({}) invalid. Must end with '/'".format(directory_path))
+
+    directory_files = os.listdir(directory_path)
+
+    all_text_records = None
+
+    for file in directory_files:
+        if file.startswith('foitext') and file.endswith(".txt"):
+            if years is None or int(file.lstrip("foitext").rstrip(".txt")) in years:
+                file_path = os.path.join(directory_path, file)
+                logging.debug("Adding file {} to dataset".format(file))
+                if all_text_records is None:
+                    # initialize
+                    all_text_records = read_maude_text_file(file_path)
+                else:
+                    all_text_records = pandas.concat([all_text_records, read_maude_text_file(file_path)],
+                                                     verify_integrity=True, ignore_index=True)
+
+    if all_text_records is None:
+        logging.error("No valid files found. Try changing the directory or the years under consideration.")
+
+    return all_text_records
 
 
 def check_bad_csv(check_file_path: str):
@@ -153,4 +222,4 @@ def check_bad_csv(check_file_path: str):
             if len(row) is not default_row_length:
                 logging.error("Error in row {}. Should have {} cols, has {}. "
                               "Row is: \n{}".format(i, len(first_row), len(row), ", ".join(row)))
-            i = i+1
+            i = i + 1
